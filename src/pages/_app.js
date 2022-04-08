b6371b4ee803fffdf6876097e44296b72f3066dc
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-04-08 15:43:03 -0300

    Atualizado instalação Material-UI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import React from 'react';
+import Head from 'next/head';
 
-export default MyApp
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+            <Component {...pageProps} />
+    </>
+  );
+}
